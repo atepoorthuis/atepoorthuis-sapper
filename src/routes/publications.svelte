47b0b2420deb7dfa7c8f32d8bb641d21da2fb5ea
Ate Poorthuis <atepoorthuis@gmail.com>
2019-09-30 00:50:03 +0800

    feat: publications subpage

---
+++ src/routes/publications.svelte
@@ -0,0 +1,15 @@
+<script>
+import Publication from '../components/Publication.svelte'
+import pubs from './_publications.js'
+</script>
+
+<h2>Publications</h2>
+{#each pubs as pub}
+	<Publication {pub} />
+{/each}
+
+<style>
+h2 {
+	@apply text-xl mb-2 font-sans font-semibold;
+}
+</style>
