bd14389e0472d0546d4891c9ff10cc8c8d96c861
Ate Poorthuis <atepoorthuis@gmail.com>
2019-09-30 00:46:50 +0800

    feat: social media links

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Nav from '../components/Nav.svelte'
 	export let segment
-	const openMenu = false
+	let openMenu = false
 </script>
 
 <style>
@@ -11,12 +11,12 @@
 </style>
 
 <div class="flex m-5 mt-8 text-gray-800 font-serif font-light">
-	<!-- <div class="block mr-4 font-sans text-base font-semibold tracking-tight">
-		<button on:click={() => openMenu ^= true} class="sm:hidden mb-4 p-2 border rounded border-teal-light">
+	<div class="block font-sans text-base font-semibold tracking-tight">
+		<button on:click={() => openMenu ^= true} class="sm:hidden mb-4 mr-4 p-2 border rounded border-teal-light">
 			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
 		</button>
-		<div class:openMenu class="w-1/5 sm:flex sm:items-center sm:w-auto hidden"><Nav {segment}/></div>
-	</div> -->
+		<div class:openMenu class="mr-8 sm:flex sm:items-center sm:w-auto hidden"><Nav {segment}/></div>
+	</div>
 	<main class="max-w-3xl container text-xs sm:text-sm md:text-base">
 		<slot></slot>
 	</main>
