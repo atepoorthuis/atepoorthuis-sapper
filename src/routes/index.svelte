c3b2a33ab1fee3b9cdcd435e933276c71d37d77d
Ate Poorthuis <atepoorthuis@gmail.com>
2019-10-06 17:45:54 +0800

    feat: add teaching summary

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,14 @@
 <script>
 import Publication from '../components/Publication.svelte'
-import pubs from './_publications.js'
+import pubs from './_recent_work.js'
 </script>
 
 <style>
 h2 {
-	@apply text-xl mt-4 mb-2 font-sans font-semibold;
+	@apply text-xl mt-5 mb-2 font-sans font-semibold;
+}
+a {
+  @apply underline;
 }
 </style>
 
@@ -15,9 +18,11 @@ h2 {
 
 <div class="intro text-justify md:text-left">
 	<p>
-	Hi, I'm Ate Poorthuis. I'm an Assistant Professor in the Humanities, Arts and
-	Social Sciences at Singapore University of Technology and Design. My research
-	explores the possibilities and limitations of big data, through quantitative
+	Hi, I'm Ate Poorthuis. I'm an Assistant Professor in the <a href="https://hass.sutd.edu.sg"
+	target="_blank" rel="noopener noreferrer">Humanities, Arts and
+	Social Sciences</a> at the <a href="https://www.sutd.edu.sg" target="_blank" rel="noopener noreferrer">
+	Singapore University of Technology and Design</a>. My research explores the possibilities and
+	limitations of big data, through quantitative
 	analysis and visualization, to better understand how our cities work. I have a
 	particular interest in the practical application of these academic insights
 	within urban planning and policy. Other projects I have worked on include the
@@ -27,7 +32,7 @@ h2 {
 </div>
 
 <div class="education">
-	<a href="#education" name="education"><h2>Education</h2></a>
+	<h2>Education</h2>
 	<div class="mb-3">
 		<p>⬡ PhD in Geography</p>
 		<p class="italic">University of Kentucky</p>
@@ -43,12 +48,35 @@ h2 {
 </div>
 
 <div class="publications">
-	<a href="#publications" name="publications"><h2>Recent Work</h2></a>
-	{#each pubs as pub}
-		<Publication {pub} />
-	{/each}
+	<h2>Recent Work</h2>
+	<ul>
+		{#each pubs as pub}
+			<li class="mb-2"><Publication {pub} /></li>
+		{/each}
+	</ul>
 </div>
 
-<!-- <a href="#projects" name="projects"><h2>Projects</h2></a> -->
-
-<!-- <a href="#teaching" name="teaching"><h2>Teaching</h2></a> -->
+<div class="teaching">
+	<h2>Teaching</h2>
+	<p class="mb-2">I teach the following courses at the
+	Singapore University of Technology and Design:</p>
+	<ul>
+		<li class="mb-2">⬡ Research Methodology for Urban Analysis (MSc 
+		<a href="https://urbanscience.sutd.edu.sg/" target="_blank" rel="noopener noreferrer">
+		Urban Science, Policy and Planning</a>)</li>
+		<li class="mb-2">⬡ Computational Urban Analysis (MSc 
+		<a href="https://urbanscience.sutd.edu.sg/" target="_blank" rel="noopener noreferrer">
+		Urban Science, Policy and Planning</a>)</li>
+		<li class="mb-2">⬡ Interactive Data Visualization (MSc 
+		<a href="https://urbanscience.sutd.edu.sg/" target="_blank" rel="noopener noreferrer">
+		Urban Science, Policy and Planning</a>)</li>
+		<li class="mb-2">⬡ Making Maps I: Introduction to Spatial Analysis and Data Visualization</li>
+		<li class="mb-2">⬡ Theorizing Society, the Self, and Culture (Introduction to Social Science)</li>
+	</ul>
+	<p class="mb-2 mt-4">Previous courses I have taught include::</p>
+	<ul>
+		<li class="mb-2">⬡ Introduction to Geographic Information Systems (University of Kentucky, University of Amsterdam)</li>
+		<li class="mb-2">⬡ Advanced Geographic Information Systems (University of Amsterdam)</li>
+		<li class="mb-2">⬡ Methods and Techniques of Spatial Planning (University of Amsterdam)</li>
+	</ul>
+</div>
